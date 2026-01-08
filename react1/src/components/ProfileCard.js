5954d9208992ed532408dd17b1427e6911fccf8c
Anshika Sharma <anshikasharma0282@gmail.com>
2026-01-08 16:08:15 +0530

    Added assignment 1 of React

---
+++ react1/src/components/ProfileCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ProfileCard(){
+    return (
+        <div>
+            <h1>Name : Anshika Sharma</h1>
+            <h1>Role : Software Developer Intern</h1>
+            <h1>Location : India</h1>
+        </div>
+    )
+}
+export default ProfileCard
+
+
